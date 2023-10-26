779df2c47a847d80263935cc00a52409c39abe21
tavora777 <tavoraguilherme4@gmail.com>
2023-10-26 15:10:22 -0300

    recebendo parametros na rota

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const { application } = require("express")
+const express = require("express")
 const path = require("path")
 
 const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
-application.get('/', (requisicao, resposta) => {
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
-application.listen(3000, () => {
+app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 
 })
